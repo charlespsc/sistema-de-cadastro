d3ae6414a76fd5ecf47aaf40b52016b9185fac4d
Charles Pereira <charles.p@edu.sc.senai.br>
2023-07-08 01:06:05 -0300

    Estou voltando a versão 1.1

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Conectando o Banco de Dados")
 //comentando o código atualizado
 console.log("Novo campo add")
-console.log("Enviando para o Github")
